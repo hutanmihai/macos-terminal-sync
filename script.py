4747c5beb6910140060259174fc3965e79b51678
mihaihutan <hutanmihai29@gmail.com>
2023-05-20 14:55:36 +0300

    Update

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from json import load as loadjson, dump as dumpjson
 
 DIRNAME = path.dirname(__file__)
 
+
 def change_background():
     with open(PATH_VSCODE_SETTINGS, 'r') as json_file:
         vsc_settings = loadjson(json_file)
@@ -13,10 +14,10 @@ def change_background():
 
 
 print('Hello, please pay attention to all the instructions'
-        , ' '
-        , '-'*50
-        , sep='\n'
-)
+      , ' '
+      , '-' * 50
+      , sep='\n'
+      )
 
 print('Enter the path to the itermcolors file: ')
 
@@ -25,27 +26,27 @@ PATH_ITERMCOLORS = input().strip()
 PATH_WITHOUT_THEME = '/'.join(PATH_ITERMCOLORS.split('/')[:-1]).strip()
 THEME_NAME = ''.join(PATH_ITERMCOLORS.split('/')[-1].split('.')[0]).strip()
 
-print('-'*50
-        , ' '
-        , sep='\n'
-)
+print('-' * 50
+      , ' '
+      , sep='\n'
+      )
 
 system(f'./tools/iterm2terminal.swift "{PATH_ITERMCOLORS}"')
 
-print(' ', 
-        '-'*50
-        , sep='\n'
-)
+print(' ',
+      '-' * 50
+      , sep='\n'
+      )
 
 movefile(f'{PATH_WITHOUT_THEME}/{THEME_NAME}.terminal', f'{DIRNAME}/generated_themes/{THEME_NAME}.terminal')
 
 print('Now I will open the new generated file for you!',
-        '-'*50
-        ,'All you have to do is to press in the upper left corner on "Shell" and then "Use Settings as Default"'
-        ,'-'*50
-        ,'After that you can close the window and the theme will be installed!'
-        , sep='\n'
-)
+      '-' * 50
+      , 'All you have to do is to press in the upper left corner on "Shell" and then "Use Settings as Default"'
+      , '-' * 50
+      , 'After that you can close the window and the theme will be installed!'
+      , sep='\n'
+      )
 
 system(f'open "{DIRNAME}/generated_themes/{THEME_NAME}.terminal"')
 
@@ -54,30 +55,32 @@ print('You are ready to go! Or maybe not? One more question, do you use VSCode? 
 USE_VSCODE = input().strip()
 
 if USE_VSCODE == 'y':
-    print('In order to have all Terminals with the same theme, you have to set the theme in the settings.json file of VSCode'
-            ,'Enter the path to the settings.json file of VSCode: '
+    print(
+        'In order to have all Terminals with the same theme, you have to set the theme in the settings.json file of VSCode'
+        , 'Enter the path to the settings.json file of VSCode: '
     )
     PATH_VSCODE_SETTINGS = input().strip()
     print('Now I will do some work for you!'
-            , '-'*50
-            , sep='\n'
-    )
+          , '-' * 50
+          , sep='\n'
+          )
 
     system(f'python3 ./tools/iterm2vsc.py "{PATH_ITERMCOLORS}" "{PATH_VSCODE_SETTINGS}"')
 
-    print('In my opinion, the background color is not always fitting well, do you want me to change it to the basic one from vscode? (y/n)')
+    print(
+        'In my opinion, the background color is not always fitting well, do you want me to change it to the basic one from vscode? (y/n)')
 
     CHANGE_BACKGROUND = input().strip().lower()
 
     if CHANGE_BACKGROUND == 'y':
         change_background()
         print('I changed the background color to the default one from vscode!'
-                , '-'*50
-                , sep='\n'
-        )
+              , '-' * 50
+              , sep='\n'
+              )
     print('-' * 100
-            , 'Have fun with your new theme!'
-            , sep='\n'
-     )
+          , 'Have fun with your new theme!'
+          , sep='\n'
+          )
 else:
     print('Have fun with your new theme!')
--- tools/iterm2vsc.py
+++ tools/iterm2vsc.py
@@ -9,34 +9,34 @@ PATH_ITERMCOLORS = sys.argv[1].strip()
 FONT = 'MesloLGS NF'
 
 MAP = {
-  'terminal.background': 'Background Color',
-  'terminal.foreground': 'Foreground Color',
-  'terminalCursor.background': 'Cursor Text Color',
-  'terminalCursor.foreground': 'Cursor Color',
-  'terminal.ansiBlack': 'Ansi 0 Color',
-  'terminal.ansiBlue': 'Ansi 4 Color',
-  'terminal.ansiBrightBlack': 'Ansi 8 Color',
-  'terminal.ansiBrightBlue': 'Ansi 12 Color',
-  'terminal.ansiBrightCyan': 'Ansi 14 Color',
-  'terminal.ansiBrightGreen': 'Ansi 10 Color',
-  'terminal.ansiBrightMagenta': 'Ansi 13 Color',
-  'terminal.ansiBrightRed': 'Ansi 9 Color',
-  'terminal.ansiBrightWhite': 'Ansi 15 Color',
-  'terminal.ansiBrightYellow': 'Ansi 11 Color',
-  'terminal.ansiCyan': 'Ansi 6 Color',
-  'terminal.ansiGreen': 'Ansi 2 Color',
-  'terminal.ansiMagenta': 'Ansi 5 Color',
-  'terminal.ansiRed': 'Ansi 1 Color',
-  'terminal.ansiWhite': 'Ansi 7 Color',
-  'terminal.ansiYellow': 'Ansi 3 Color',
-  'terminal.selectionBackground':'Selection Color'
+    'terminal.background': 'Background Color',
+    'terminal.foreground': 'Foreground Color',
+    'terminalCursor.background': 'Cursor Text Color',
+    'terminalCursor.foreground': 'Cursor Color',
+    'terminal.ansiBlack': 'Ansi 0 Color',
+    'terminal.ansiBlue': 'Ansi 4 Color',
+    'terminal.ansiBrightBlack': 'Ansi 8 Color',
+    'terminal.ansiBrightBlue': 'Ansi 12 Color',
+    'terminal.ansiBrightCyan': 'Ansi 14 Color',
+    'terminal.ansiBrightGreen': 'Ansi 10 Color',
+    'terminal.ansiBrightMagenta': 'Ansi 13 Color',
+    'terminal.ansiBrightRed': 'Ansi 9 Color',
+    'terminal.ansiBrightWhite': 'Ansi 15 Color',
+    'terminal.ansiBrightYellow': 'Ansi 11 Color',
+    'terminal.ansiCyan': 'Ansi 6 Color',
+    'terminal.ansiGreen': 'Ansi 2 Color',
+    'terminal.ansiMagenta': 'Ansi 5 Color',
+    'terminal.ansiRed': 'Ansi 1 Color',
+    'terminal.ansiWhite': 'Ansi 7 Color',
+    'terminal.ansiYellow': 'Ansi 3 Color',
+    'terminal.selectionBackground': 'Selection Color'
 }
 
 iterm_theme_values_list = xmltodict(open(PATH_ITERMCOLORS, 'r').read())['plist']['dict']['dict']
 iterm_theme_keys_list = xmltodict(open(PATH_ITERMCOLORS, 'r').read())['plist']['dict']['key']
 
 vsc_theme = {}
- 
+
 IDK = {}
 
 counter = 0
@@ -46,9 +46,9 @@ for dict in iterm_theme_values_list:
         b, g, r = dict['real']
     else:
         a, b, g, r = dict['real']
-    blue  = floor(float(b) * 255)
+    blue = floor(float(b) * 255)
     green = floor(float(g) * 255)
-    red   = floor(float(r) * 255)
+    red = floor(float(r) * 255)
     IDK[iterm_theme_keys_list[counter]] = '#%02x%02x%02x' % (red, green, blue)
     counter += 1
 
@@ -60,13 +60,15 @@ for key in MAP:
 with open(PATH_VSCODE_SETTINGS, 'r') as json_file:
     vsc_settings = loadjson(json_file)
 
+if 'workbench.colorCustomizations' not in vsc_settings:
+    vsc_settings['workbench.colorCustomizations'] = {}
+
 # Replace the colors in the settings.json file with the converted values
 for key in vsc_theme:
-        vsc_settings['workbench.colorCustomizations'][key] = vsc_theme[key]
+    vsc_settings['workbench.colorCustomizations'][key] = vsc_theme[key]
 
 vsc_settings['terminal.integrated.fontFamily'] = FONT
 
 # Write the new settings.json file to the path specified in VSC_SETTINGS_PATH
 with open(PATH_VSCODE_SETTINGS, 'w') as json_file:
     dumpjson(vsc_settings, json_file, indent=4)
-    
